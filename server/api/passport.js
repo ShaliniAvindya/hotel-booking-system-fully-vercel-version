9df7110196d482b291343b1593468132a3253f59
ShaliniAvindya <132177735+ShaliniAvindya@users.noreply.github.com>
2025-06-16 01:53:38 +0530

    Update passport.js

--- server/api/passport.js
+++ server/api/passport.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
-const User = require('./models/user');
+const User = require('../models/user');
 require('dotenv').config();
 
 // Google Strategy
