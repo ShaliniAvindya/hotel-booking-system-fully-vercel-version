0567aade18e7560be8c53472b71955482a1337cb
ShaliniAvindya <132177735+ShaliniAvindya@users.noreply.github.com>
2025-06-16 00:35:45 +0530

    Add files via upload

---
+++ server/models/Room.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const roomSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    maxCount: { type: Number, required: true },
+    rentPerDay: { type: Number, required: true },
+    imageUrls: { type: [String], required: true },
+    phoneNumber: { type: String, required: true },
+    type: { type: String, required: true },
+    description: { type: String, required: true },
+    currentBookings: { type: [Object], default: [] },
+    facilities: { type: [String], required: true },
+});
+
+module.exports = mongoose.model('Room', roomSchema);
