828789454b87f7adf449eae264d198cfb5adb967
ShaliniAvindya <132177735+ShaliniAvindya@users.noreply.github.com>
2025-07-17 09:34:21 +0530

    Update Bookings.jsx

--- client/src/components/Bookings.jsx
+++ client/src/components/Bookings.jsx
@@ -16,7 +16,7 @@ const Bookings = () => {
     updatedBookings[index].status = value;
     setBookings(updatedBookings);
     axios
-      .put(`https://lushhotels.com.lk/api/book/update-status/${updatedBookings[index]._id}`, {
+      .put(`https://hotel-booking-system-fully-vercel-v-sigma.vercel.app/api/book/update-status/${updatedBookings[index]._id}`, {
         status: value,
       })
       .catch((err) => console.error('Error updating status:', err));
