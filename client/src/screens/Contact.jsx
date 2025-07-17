837105c95bc69eb7753c3868341c1ff97f3322f2
ShaliniAvindya <132177735+ShaliniAvindya@users.noreply.github.com>
2025-07-17 09:30:39 +0530

    Update Contact.jsx

--- client/src/screens/Contact.jsx
+++ client/src/screens/Contact.jsx
@@ -23,7 +23,7 @@ export default function MaldivesContactPage() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await fetch('https://lushhotels.com.lk/api/contact', {
+      await fetch('https://hotel-booking-system-fully-vercel-v-sigma.vercel.app/api/contact', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
